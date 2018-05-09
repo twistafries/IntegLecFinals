6bd2fece5f46d6b9597f70c6361c014b5b0627b2
Cess <>
2018-05-09 20:09:36 +0800

    change and alter the code for python client

--- Python Client/Client.py
+++ Python Client/Client.py
@@ -1,6 +1,5 @@
-import sys
-from omniORB import CORBA
-
-import Hangman_Game
-
-orb = CORBA.ORB_init()
+>>> import CORBA, Hangman_Game
+>>> orb = CORBA.ORB_init()
+>>> o = orb.string_to_object("http://localhost:9001")
+>>> o = o._narrow(Hangman_Game.(*Insert the CookieServer))
+>>> print o.get_cookie()
